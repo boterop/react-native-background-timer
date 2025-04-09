5d8a6b3e2ae26e57cb96f9c0bd2c11f30c78cbfe
boterop <boterop22@gmail.com>
2025-04-09 16:07:57 -0500

    Format document

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,39 @@
-import {
-    EmitterSubscription,
-    TurboModule,
-} from 'react-native';
+import { EmitterSubscription, TurboModule } from 'react-native';
 
 declare module '@boterop/react-native-background-timer' {
-    export interface TimerCallback {
-        callback: () => void;
-        interval: boolean;
-        timeout: number;
-    }
+	export interface TimerCallback {
+		callback: () => void;
+		interval: boolean;
+		timeout: number;
+	}
 
-    export interface RNBackgroundTimerModule extends TurboModule {
-        start: (delay?: number) => void;
-        stop: () => void;
-        setTimeout: (id: number, timeout: number) => void;
-        clearTimeout?: (id: number) => void;
-    }
+	export interface RNBackgroundTimerModule extends TurboModule {
+		start: (delay?: number) => void;
+		stop: () => void;
+		setTimeout: (id: number, timeout: number) => void;
+		clearTimeout?: (id: number) => void;
+	}
 
-    export interface BackgroundTimerInterface {
-        uniqueId: number;
-        callbacks: Record<number, TimerCallback>;
-        backgroundTimer?: number;
-        backgroundListener?: EmitterSubscription;
+	export interface BackgroundTimerInterface {
+		uniqueId: number;
+		callbacks: Record<number, TimerCallback>;
+		backgroundTimer?: number;
+		backgroundListener?: EmitterSubscription;
 
-        start: (delay?: number) => void;
-        stop: () => void;
+		start: (delay?: number) => void;
+		stop: () => void;
 
-        runBackgroundTimer: (callback: () => void, delay: number) => void;
-        backgroundClockMethod: (callback: () => void, delay: number) => void;
-        stopBackgroundTimer: () => void;
+		runBackgroundTimer: (callback: () => void, delay: number) => void;
+		backgroundClockMethod: (callback: () => void, delay: number) => void;
+		stopBackgroundTimer: () => void;
 
-        setTimeout: (callback: () => void, timeout: number) => number;
-        clearTimeout: (timeoutId: number) => void;
+		setTimeout: (callback: () => void, timeout: number) => number;
+		clearTimeout: (timeoutId: number) => void;
 
-        setInterval: (callback: () => void, timeout: number) => number;
-        clearInterval: (intervalId: number) => void;
-    }
+		setInterval: (callback: () => void, timeout: number) => number;
+		clearInterval: (intervalId: number) => void;
+	}
 
-    const BackgroundTimer: BackgroundTimerInterface;
-    export default BackgroundTimer;
+	const BackgroundTimer: BackgroundTimerInterface;
+	export default BackgroundTimer;
 }
